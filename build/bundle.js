106bd92c5566a6f8f2dc22ff79aded7067b64284
nguyen phuoc <quangphuoc@jv-it.com.vn>
2021-01-20 21:35:13 +0700

    add webpack

---
+++ build/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log(8),console.log(16)})();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import sum from "./main";
+import { sub } from "./sub";
+const total = sum(3, 5);
+console.log(total);
+console.log(sub(10, 6));
